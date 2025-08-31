d8f7b6eb247342cb9dd5357e22ddb9852f9aa62c
huhonmcmc <huhonmcmc@gmail.com>
2025-08-31 15:57:17 +0900

    image_server :: cors 설정

---
+++ src/main/java/nuri/image_server/global/config/WebConfig.java
@@ -0,0 +1,24 @@
+package nuri.image_server.global.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000"
+                )
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization", "X-Access-Token")
+                .allowCredentials(true);
+    }
+}
